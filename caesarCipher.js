cec16d224ebb93a969db35d36af46d1d5c560ab5
alecnissen <alecnissen@yahoo.com>
2023-07-04 21:32:54 -0400

    started over and trying to figure out the final logic

--- caesarCipher.js
+++ caesarCipher.js
@@ -52,45 +52,140 @@
 
 // another loop, set aA to 0 and keeping looping? nested loop? 
 
-// 
+// I will just do the period puncuation, then write the actual test, 
+
+// then move on. 
 
 
 
-let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+// let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-function caesarCipher(str, shift) {
-    let result = ''; 
+ // commented out below here 
+// function caesarCipher(str, shift) {
+//     let result = ''; 
 
-    let shiftFactor = shift; 
+//     let shiftFactor = shift; 
+ 
+//     for (let i = 0; i < str.length; i++) {
 
-    for (let i = 0; i < str.length; i++) {
+//         // let baseCodeUpperCase = 65;
 
-        let numberCharacter = str[i].charCodeAt(); 
+//         // let baseCodeLowerCase = 97;
 
-        console.log(numberCharacter);
+//         let numberCharacter = str[i].charCodeAt(); 
+//         console.log(numberCharacter);
+//         let shiftedCharacters = (numberCharacter + shiftFactor);
+ 
+        // I am trying to figure out the correct cipher for uppercase and lowercase letters 
 
-        let shiftedCharacters = (numberCharacter + shiftFactor);
+        // so far I made a conditional using regex that tests for uppercase letters 
 
-        if (shiftedCharacters >= 90) { 
-            shiftedCharacters = (shiftedCharacters % 90) + 64;
+        // passing the string into the regex pattern, 
+
+        // then subtracting the differnce between the number character and the base code 
+
+        // then adding the base case back in. 
+
+        // then how do I apply the shift? 
+
+        // I don't understand why I am USING SUBTRACTION THEN ADDING THE BASE IN!!!!
+
+        // I DONT UNDERSTAND THIS AT ALL. 
+
+        // this conditional literally makes no sense, and I don't understand this process at all 
+
+        // 
+
+        // if (/[A-Z]/.test(str)) { 
+
+        //     // console.log(str); 
+
+        //     let upperCaseDifference = numberCharacter - baseCodeUpperCase;
+          
+        //      upperCaseDifference = upperCaseDifference + baseCodeUpperCase;
+
+        //      console.log(upperCaseDifference); 
+
+        //     //  let returnShiftedLetter = String.fromCharCode(upperCaseDifference);
+
+        //     //  result += returnShiftedLetter;
+        //      // what am I TESTINGGGGGGGG!?!?!!??!? 
+
+        //      // to prevent wrapping 
+
+        //      // 
+
+        //     //  shiftedCharacters = upperCaseDifference; 
+
+        //     //  console.log(upperCaseDifference);
+        //   }
+
+
+        // commented out below here 
+          
+//         let returnShiftedLetter = String.fromCharCode(shiftedCharacters); 
+
+//         result += returnShiftedLetter;
+//     } 
+//     return result;
+// }  
+
+// console.log(caesarCipher('abc', 3));
+
+
+
+// function caesarcipher(letter, shift) { 
+
+//      let letterCharCode = letter.charCodeAt(); 
+//      let shiftedCharCode = letter + shift; 
+
+//     if (letterCharCode >= 65 && letterCharCode <= 90) { 
+//         // if letter is uppercase, if letter is within the range of uppercase (65 <= letter <= 90)
+       
+//         if (shiftedCharCode > 90) { 
+//             let remainder = (shiftedCharCode - 90);
+//             let loopedOverCharCode = 64 + remainder;
+//             let newLetter = String.fromCharCode(loopedOverCharCode); 
+//             return newLetter;
+//         }
+//       } else if (shiftedCharCode <= 90) { 
+//         let newLetter = String.fromCharCode(shiftedCharCode);
+//         return newLetter;
+//       } else if (letter >= 97 && letter <= 122) { 
+//         let newLetter = String.fromCharCode(shiftedCharCode); 
+//         return newLetter;
+//       }
+//     }  
+
+function caesarcipher(letter, shift) {
+    let letterCharCode = letter.charCodeAt();
+    let shiftedCharCode = letterCharCode + shift;
+    
+    if (letterCharCode >= 65 && letterCharCode <= 90) {  // if letter is uppercase, if letterCode is within the range of uppercase (65 <= letterCode <= 90)
+        if (shiftedCharCode > 90) {
+             let remainder = (shiftedCharCode - 90);
+             let loopedOverCharCode = 64 + remainder;
+             let newLetter = String.fromCharCode(loopedOverCharCode);
+             return newLetter;
+          } else if ( shiftedCharCode <= 90) {
+                let newLetter = String.fromCharCode(shiftedCharCode);
+                return newLetter;
+             }
+      } else if (letterCharCode >= 97 && letterCharCode <= 122) { // if letter is lowercase, if letter is within the range of lowercase (97 <= letter <= 122)
+        //some code here
+        if (shiftedCharCode > 122) { 
+            let remainder = (shiftedCharCode - 122);
+            let loopedOverCharCode = 97 + remainder;
+            let newLetter =  String.fromCharCode(loopedOverCharCode);
+            return newLetter;
         } 
 
-        else if (shiftedCharacters >= 122) {
-            shiftedCharacters = (shiftedCharacters % 122) + 96;
-         
-        } 
+      }
+    }
 
-        
-        let returnShiftedLetter = String.fromCharCode(shiftedCharacters); 
 
-        result += returnShiftedLetter;
 
-    } 
-
-    return result;
-}  
-
-console.log(caesarCipher('ZAB', 3));
+    console.log(caesarcipher('a', 3));
 
 
 
@@ -102,3 +197,33 @@ console.log(caesarCipher('ZAB', 3));
 // see if you can, loop through them again, 
 
 // post the alphabet again, 
+
+
+
+       // lower case
+        //  if (shiftedCharacters >= 122) {
+        //     shiftedCharacters = (shiftedCharacters % 122) + 96;
+        //     console.log(shiftedCharacters);
+        // }  
+        // uppercase 
+
+        // if (numberCharacter >= 65) { 
+        //     let upperCaseDifference = numberCharacter - baseCodeUpperCase;
+
+        //     upperCaseDifference = upperCaseDifference + baseCodeUpperCase;
+
+        //     console.log(upperCaseDifference); 
+            
+        // } else if (numberCharacter >= 97) { 
+        //     let lowerCaseDifference = numberCharacter - baseCodeLowerCase
+
+        //     console.log(lowerCaseDifference);
+
+        // } 
+
+        // the problem is I need my conditionals to check if we have gone 
+
+        // over the alphabet, so restart from the beginnging. 
+
+        // take the difference of the 
+        // 
